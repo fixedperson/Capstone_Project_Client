fa090f42097e09e636bd39833a893e8bed4aac0e
tmdgjs306 <58868717+tmdgjs306@users.noreply.github.com>
2023-05-18 13:08:31 +0900

    AWS 접속 구현

--- Assets/Scripts/Managers/NetworkManager.cs
+++ Assets/Scripts/Managers/NetworkManager.cs
@@ -20,11 +20,16 @@ public class NetworkManager
         // DNS (Domain Name System)
         string host = Dns.GetHostName();
         IPHostEntry ipHost = Dns.GetHostEntry(host);
-        IPAddress ipAddr = ipHost.AddressList[0];
+        
+        //로컬 호스트 접속 IP 주소 
+        //IPAddress ipAddr = ipHost.AddressList[0];
+        
+        //AWS 서버 접속 IP 주소 
+        IPAddress ipAddr = IPAddress.Parse("3.39.123.19");
         IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
         Connector connector = new Connector();
-
+        
         connector.Connect(endPoint,
             () => { return _session; },
             1);
