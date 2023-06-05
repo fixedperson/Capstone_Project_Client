2bfe9330f9530a5513d238942109e927c5279f50
fixedperson <fixedperson1@gmail.com>
2023-06-05 15:10:10 +0900

    aws IP 주소 변경

--- Assets/Scripts/Managers/NetworkManager.cs
+++ Assets/Scripts/Managers/NetworkManager.cs
@@ -25,7 +25,7 @@ public class NetworkManager
         // IPAddress ipAddr = ipHost.AddressList[0];
         
         //AWS 서버 접속 IP 주소 
-        IPAddress ipAddr = IPAddress.Parse("3.39.123.19");
+        IPAddress ipAddr = IPAddress.Parse("13.125.121.143");
         IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
         Connector connector = new Connector();
